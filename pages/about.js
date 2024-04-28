68de991254edcfc1f9b71854afb07bf056e77557
vizucode <havisikkubaru@gmail.com>
2024-04-28 11:25:23 +0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import Paragraph from '../components/text/Paragraph'
+import Link from 'next/link'
 import Title from '../components/text/Title'
 import MetaSeo from '../components/MetaSeo'
 
